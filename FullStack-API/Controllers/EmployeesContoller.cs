a64f889fa7df28b172cbb95d0298b17331c96014
SayanGithub1 <sayanmondal2002@gmail.com>
2024-02-28 23:13:20 +0530

    added getall method

---
+++ FullStack-API/Controllers/EmployeesContoller.cs
@@ -0,0 +1,23 @@
+﻿using FullStack_API.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace FullStack_API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EmployeesContoller : Controller
+    {
+        private readonly FullStackDbContext _fullStackDbContext;
+        public EmployeesContoller(FullStackDbContext fullStackDbContext) 
+        {
+            _fullStackDbContext = fullStackDbContext;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAllEmployees()
+        {
+          var employees = await  _fullStackDbContext.Employees.ToListAsync();
+            return Ok(employees);
+        }
+    }
+}
